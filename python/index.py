7f982c081406dd9b040bb115414363f4fc983f70
Quentin <91956889+SadeQL@users.noreply.github.com>
2021-12-17 16:00:51 +0100

    on a réussi

--- python/index.py
+++ python/index.py
@@ -2,18 +2,6 @@ from math import nan
 import random
 
 arr = ["red", "blue", "yellow", "green", "purple", "orange", "pink", "maroon"]
-#secret_code = random.sample(arr, k=4)
-#guess = input("Give 4 colors: ")
-#first_try = guess.split(', ')
-
-
-def user_color():
-    guess = input("Give 4 colors: ")
-    first_try = guess.split(', ')
-    return first_try
-
-
-user_color()
 
 
 def intersection(lst1, lst2):
@@ -21,13 +9,13 @@ def intersection(lst1, lst2):
     return lst3
 
 
-def check_list(first_try, secret_code):
-    matches = intersection(first_try, secret_code)
+def check_list(arr1, arr2):
+    matches = intersection(arr1, arr2)
     bad_order = len(matches)
     good_color_count = 0
     if matches != []:
-        for i in range(0, len(first_try)):
-            if first_try[i] == secret_code[i]:
+        for i in range(0, len(arr1)):
+            if arr1[i] == arr2[i]:
                 good_color_count += 1
                 bad_order -= 1
     else:
@@ -38,15 +26,20 @@ def check_list(first_try, secret_code):
 
 def play_turn():
     secret_code = random.sample(arr, k=4)
-    first_try = user_color()
     i = 1
     while i < 10:
+        guess = input("Give 4 colors: ")
+        first_try = guess.split(', ')
         i += 1
+        print(first_try)
+        print(check_list(first_try, secret_code))
+
         if check_list(first_try, secret_code) == (4, 4):
             break
-        else:
-            user_color()
-    return first_try
+
+    print(first_try)
+    print(secret_code)
+    print(check_list(first_try, secret_code))
 
 
-print(check_list(first_try, secret_code))
+play_turn()
